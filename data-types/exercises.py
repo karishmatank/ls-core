819607fc7de3386ff63107cd8ee33ab4dd99121f
Karishma Tank <tankkarishma@gmail.com>
2025-09-10 16:43:45 -0400

    Data types chapter

---
+++ data-types/exercises.py
@@ -0,0 +1,17 @@
+# Question 2: Create a tuple called names with given pet names
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin'
+)
+
+# Question 3: Create a dictionary called pets that contains given pet names and types of animals
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard'
+}
