8a29bbbd881d34adc02c335c7f7d539885ca7875
Karishma Tank <tankkarishma@gmail.com>
2025-09-11 15:03:51 -0400

    Intro to collections chapter

---
+++ intro-to-collections/q11.py
@@ -0,0 +1,10 @@
+data = {
+    'Alice': 'USA',
+    'Francois': 'Canada',
+    'Inti': 'Peru',
+    'Monika': 'Germany',
+    'Sanyu': 'Uganda',
+    'Yoshitaka': 'Japan',
+}
+
+print(data['Alice'])
