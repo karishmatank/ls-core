f3f31c475ee575535397b6b807bd11cc638a8acf
Karishma Tank <tankkarishma@gmail.com>
2025-09-19 16:04:14 -0400

    RPS: Adds best of 5 rounds, improves input validation

--- py-101/lesson_2/rock_paper_scissors.py
+++ py-101/lesson_2/rock_paper_scissors.py
@@ -10,18 +10,19 @@ USER_WIN_SCENARIOS = {
     "lizard": ["paper", "spock"],
     "spock": ["rock", "scissors"]
 }
+MAX_WINS = 3
 
 def prompt(message):
     print(f"==> {message}")
 
 def get_user_choice():
     while True:
-        choice = input()
-        if choice.lower() in VALID_CHOICES:
+        choice = input().lower().strip()
+        if choice in VALID_CHOICES:
             break
 
         # Check if user used an abbreviation
-        if choice.lower() in ABBREVIATIONS:
+        if choice in ABBREVIATIONS:
             # Transform user choice into full string
             choice = VALID_CHOICES[ABBREVIATIONS.index(choice.lower())]
             break
@@ -35,27 +36,53 @@ def get_computer_choice():
 def determine_winner(user, computer):
     if user == computer:
         return None
-    elif computer in USER_WIN_SCENARIOS[user]:
+    if computer in USER_WIN_SCENARIOS[user]:
         return "user"
-    else:
-        return "computer"
-    
-def print_winner(winner):
+    return "computer"
+
+def print_round_winner(winner):
     if winner == "user":
-        prompt("You win!")
+        prompt("You win this round!")
     elif winner == "computer":
-        prompt("Computer wins!")
+        prompt("Computer wins this round!")
     else:
-        prompt("It's a tie.")
+        prompt("This round is a tie.")
+
+def print_game_winner(user, computer):
+    if user > computer:
+        prompt("You win the game!!!")
+    elif user < computer:
+        prompt("Computer wins the game!!!")
+    else:
+        prompt("Something went wrong.")
+
+def print_score(user, computer):
+    prompt(f"User has {user} wins, computer has {computer} wins.")
 
 def play_game():
-    prompt(f"Choose one- {', '.join(VALID_CHOICES)}\n" +
-           f"    You may also abbreviate to {', '.join(ABBREVIATIONS)}: ")
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    prompt(f"You chose {user_choice}, computer chose {computer_choice}")
-    winner = determine_winner(user_choice, computer_choice)
-    print_winner(winner)
+    user_wins = 0
+    computer_wins = 0
+
+    prompt("We're playing best to 5, let's start!")
+
+    while user_wins < MAX_WINS and computer_wins < MAX_WINS:
+        prompt(f"Choose one- {', '.join(VALID_CHOICES)}\n" +
+            f"    You may also abbreviate to {', '.join(ABBREVIATIONS)}: ")
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        prompt(f"You chose {user_choice}, computer chose {computer_choice}")
+
+        winner = determine_winner(user_choice, computer_choice)
+        print_round_winner(winner)
+
+        if winner == 'user':
+            user_wins += 1
+        elif winner == 'computer':
+            computer_wins += 1
+
+        print_score(user_wins, computer_wins)
+
+    print_game_winner(user_wins, computer_wins)
 
 while True:
     play_game()
