dc49b65d32c792499c3dca89a0ef980947b5f4e4
Karishma Tank <tankkarishma@gmail.com>
2026-02-09 20:53:18 -0500

    PY185 todos app

---
+++ py-185/todos_final/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE lists (
+  id serial PRIMARY KEY,
+  title text UNIQUE NOT NULL
+);
+
+CREATE TABLE todos (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  completed boolean NOT NULL DEFAULT false,
+  list_id integer NOT NULL REFERENCES lists (id) ON DELETE CASCADE
+);
